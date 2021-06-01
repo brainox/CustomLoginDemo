aad71ee26d5ae7514e1d92ed66e995c4a65b124a
brainox <macmartins081@gmail.com>
2021-06-01 17:02:53 +0100

    created the root view controller

---
+++ CustomLoginDemo/View Controllers/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  CustomLoginDemo
+//
+//  Created by Decagon on 01/06/2021.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ CustomLoginDemo/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomLoginDemo
+//
+//  Created by Decagon on 01/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
