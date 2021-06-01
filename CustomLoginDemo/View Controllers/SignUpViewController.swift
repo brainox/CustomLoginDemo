abe9826dde4be6e6b83481dc9eddb5b91159bf82
brainox <macmartins081@gmail.com>
2021-06-01 20:13:08 +0100

    added firebase

--- CustomLoginDemo/View Controllers/SignUpViewController.swift
+++ CustomLoginDemo/View Controllers/SignUpViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpElements()
     }
     
-
+    func setUpElements() {
+        errorLabel.alpha = 0
+    
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+        // validate the fields
+        
+        // Create the user
+        
+        // Transition to the home screen
+    }
+    
 }
